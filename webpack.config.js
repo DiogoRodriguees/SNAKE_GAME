5f2b6d3e421a348bc8160d8f3f8964c501fccda8
diogorodriguees <diogorodrigueslife@gmail.com>
2023-06-18 15:26:43 -0300

    Revert ":rocket: tentativa de adicionar o node"
    This reverts commit eabeb3ec7d664a38b27354fbc78203ce922c3266.

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-module.exports = {
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-        }),
-    ],
-};
